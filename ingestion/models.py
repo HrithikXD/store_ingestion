fdfd966b2f31f5d331d5885411bcdf11be04d1eb
GNRTRREX <hrithikhh86@gmail.com>
2026-05-03 15:09:01 +0530

    Model complete

--- ingestion/models.py
+++ ingestion/models.py
@@ -25,7 +25,7 @@ class Store(models.Model):
     name = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     store_brand = models.ForeignKey(StoreBrand, null=True, on_delete=models.SET_NULL)
-    store_type = models.ForeignKey(StoreType,null=True, on_delete=models.SET_NULL)
+    store_type = models.ForeignKey(StoreType, null=True, on_delete=models.SET_NULL)
     city = models.ForeignKey(City,null=True, on_delete=models.SET_NULL)
     state = models.ForeignKey(State, null=True, on_delete=models.SET_NULL)
     country = models.ForeignKey(Country, null=True, on_delete=models.SET_NULL)
@@ -36,4 +36,25 @@ class Store(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
 
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, default='')
+    last_name = models.CharField(max_length=150, default='')
+    email = models.EmailField(max_length=254)
+    user_type = models.IntegerField(default=1)
+    phone_number = models.CharField(max_length=32, default='')
+    supervisor = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+    is_active = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
 
+class PermanentJourneyPlan(models.Model):
+    user    = models.ForeignKey(User, on_delete=models.CASCADE)
+    store   = models.ForeignKey(Store, on_delete=models.CASCADE)
+    date       = models.DateField(null=True)
+    is_active  = models.BooleanField(default=True)
+    created_on  = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'store', 'date')
